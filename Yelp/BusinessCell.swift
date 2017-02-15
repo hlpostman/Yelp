e20025a3393f822e4927efbaa0f02eda532f05fc
Hannah Lily Postman <postman.hl@gmail.com>
2017-02-14 16:23:16 -0800

    Video 1 complete.  Outlets are connected, and data is printing correctly, but simulator is not loading data to view - only placeholders appear in all cells.

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,52 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Aristotle on 2017-02-14.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var thumbImageView: UIImageView!
+    @IBOutlet weak var ratingsImageView: UIImageView!
+    
+    @IBOutlet weak var businessNameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    
+    var business: Business! {
+        didSet {
+            // Images
+            thumbImageView.setImageWith(business.imageURL!)
+            ratingsImageView.setImageWith(business.ratingImageURL!)
+            
+            // Labels
+            businessNameLabel.text = business.name
+            distanceLabel.text = business.distance
+            addressLabel.text = business.address
+            categoriesLabel.text = business.categories
+            reviewCountLabel.text = "\(business.reviewCount!) Reviews"
+            
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbImageView.layer.cornerRadius = 3
+        thumbImageView.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
