feb2dbdc6bf8021ca0718c9292986d376c5699ce
Hannah Lily Postman <postman.hl@gmail.com>
2017-02-17 23:37:37 -0800

    Add basic search-within-results functionality

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -11,19 +11,18 @@ import UIKit
 class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     var businesses: [Business]!
+    var searchedBusinesses: [Business]?
     let searchBar = UISearchBar()
     
     @IBOutlet weak var tableView: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return businesses?.count ?? 0
+        return searchedBusinesses?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
-        
-        cell.business = businesses[indexPath.row]
-        
+        cell.business = searchedBusinesses?[indexPath.row]
         return cell
     }
     
@@ -46,10 +45,12 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
             navigationBar.barTintColor = UIColor(red: 0.7, green: 0.03, blue: 0.03, alpha: 1)
 
         }
-    
+        
+        // Filter results loaded
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.searchedBusinesses = self.businesses
             self.tableView.reloadData()
             
             if let businesses = businesses {
@@ -73,6 +74,24 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
          }
          */
         
+    }
+    
+    // Filter within results
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            searchedBusinesses = self.businesses
+            tableView.reloadData()
+        } else {
+//            searchedBusinesses = businesses!.filter({ (business) -> bool in
+//                return (business as! Business).name?.hasPrefix(searchText)
+//                })
+            searchedBusinesses = businesses.filter({ (dataItem: Business) -> Bool in
+                print(dataItem.name!)
+            return dataItem.name!.hasPrefix(searchText)
+        })
+                
+        tableView.reloadData()
+        }
     }
     
     override func didReceiveMemoryWarning() {
